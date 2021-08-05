17bcee4f498fb6ef05dd2522c471e70f8732a30e
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-08-05 00:42:58 -0600

    Se diferencian simbolos y caracteres y se muestra en ventana

--- src/leer/LeerCadena.java
+++ src/leer/LeerCadena.java
@@ -16,22 +16,43 @@ public class LeerCadena {
       public static void leer() {
         String cadena = Interfaz.textoIngresado.getText();
         String simbolo;
+        String caractere;
+        String id;
+        int entero;
+        double decimal;
+        
         String cadenaSimbolos = "";
+        String cadenaCaracteres = "";
+        String cadenaId = "";
+        String cadenaNumeros = "";
+        String cadenaDecimales = "";
 
         for (int i = 0; i < cadena.length(); i++) {
 
             String caracter = String.valueOf(cadena.charAt(i));
 
             if (caracter.equals("*") || caracter.equals("/") || caracter.equals("+") || caracter.equals("-") || caracter.equals("(") || caracter.equals(")") || caracter.equals("{") || caracter.equals("}")
-                   || caracter.equals("[") || caracter.equals("]")) {
+                   || caracter.equals("[") || caracter.equals("]")|| caracter.equals("?")|| caracter.equals("¿")|| caracter.equals("!")|| caracter.equals("¡")|| caracter.equals("]")|| caracter.equals("<")|| caracter.equals(">")) {
 
                 System.out.println(TipoToken.SIMBOLO.toString());
                 System.out.println(cadena.charAt(i));
                 cadenaSimbolos = cadenaSimbolos + cadena.charAt(i);
                 System.out.println(cadenaSimbolos);
-                Interfaz.salida.setText(TipoToken.SIMBOLO.getTipo() + " " + cadenaSimbolos);
+                
             }
+            
+               if (caracter.equals(".") || caracter.equals(",") || caracter.equals(";") || caracter.equals(":") || caracter.equals("´") || caracter.equals("'") || caracter.equals("_") || caracter.equals("@")
+                   || caracter.equals("#") || caracter.equals("%")|| caracter.equals("=")) {
 
+                System.out.println(TipoToken.CARACTER.toString());
+                System.out.println(cadena.charAt(i));
+                cadenaCaracteres = cadenaCaracteres + cadena.charAt(i);
+                System.out.println(cadenaCaracteres);
+                
+            }
+            
+            Interfaz.salida.setText(TipoToken.CARACTER.getTipo() + " " + cadenaCaracteres+"\n"+TipoToken.SIMBOLO.getTipo() + " " + cadenaSimbolos);
+            //Interfaz.salida.setText();
         }
     }
 }
