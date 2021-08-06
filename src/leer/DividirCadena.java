a0a3fdf3941a1b236da47beeff62619184c0aa77
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-08-05 22:51:50 -0600

    Correccion validar simbolos

--- src/leer/DividirCadena.java
+++ src/leer/DividirCadena.java
@@ -14,6 +14,18 @@ import interfaz.Interfaz;
  */
 public class DividirCadena {
 
+    private static String cadenaSimbolos = "";
+    private static String cadenaCaracteres = "";
+    private static String cadenaId = "";
+    private static String cadenaEnteros = "";
+    private static String cadenaDecimales = "";
+
+    private static String cadenaFSimbolos = "";
+    private static String cadenaFCaracteres = "";
+    private static String cadenaFId = "";
+    private static String cadenaFEnteros = "";
+    private static String cadenaFDecimales = "";
+
     public static void leerCadena() {
 
         String cadena = Interfaz.textoIngresado.getText();
@@ -31,38 +43,64 @@ public class DividirCadena {
 
     public static void Clasificar(String dato) {
 
-        String simbolo;
-        String caractere;
-        String id;
-        int entero;
-        double decimal;
-
-        String cadenaSimbolos = "";
-        String cadenaCaracteres = "";
-        String cadenaId = "";
-        String cadenaEnteros = "";
-        String cadenaDecimales = "";
-
         for (int i = 0; i < dato.length(); i++) {
-            
+
             String caracter = String.valueOf(dato.charAt(i));
 
-            if (caracter.equals("*") || caracter.equals("/") || caracter.equals("+") || caracter.equals("-") || caracter.equals("(") || caracter.equals(")") || caracter.equals("{") || caracter.equals("}")|| caracter.equals("[") || caracter.equals("]") || caracter.equals("?") || caracter.equals("¿") || caracter.equals("!") || caracter.equals("¡") || caracter.equals("]") || caracter.equals("<") || caracter.equals(">")) {
-                System.out.println(TipoToken.SIMBOLO.toString());
-                System.out.println(dato.charAt(i));
+            if (caracter.equals("*") || caracter.equals("/") || caracter.equals("+") || caracter.equals("-") || caracter.equals("(") || caracter.equals(")") || caracter.equals("{") || caracter.equals("}") || caracter.equals("[") || caracter.equals("]") || caracter.equals("?") || caracter.equals("¿") || caracter.equals("!") || caracter.equals("¡") || caracter.equals("]") || caracter.equals("<") || caracter.equals(">")) {
+
                 cadenaSimbolos = cadenaSimbolos + dato.charAt(i);
-                System.out.println(cadenaSimbolos);
+                cadenaFSimbolos = cadenaSimbolos;
+                System.out.println("Final simbolos "+cadenaFSimbolos);
 
             }
 
-            if (dato.equals("b")) {
-                System.out.println("soy una b");
+            if (caracter.equals(".") || caracter.equals(",") || caracter.equals(";") || caracter.equals(":") || caracter.equals("´") || caracter.equals("'") || caracter.equals("_") || caracter.equals("@")
+                    || caracter.equals("#") || caracter.equals("%") || caracter.equals("=")) {
+
+                System.out.println(TipoToken.CARACTER.toString());
+                System.out.println(dato.charAt(i));
+                cadenaCaracteres = cadenaCaracteres + dato.charAt(i);
+                cadenaFCaracteres = cadenaCaracteres;
+                System.out.println(cadenaFCaracteres);
+            }
+
+            /* if (validarNumero(dato) == true) {
+                
+                System.out.println(TipoToken.NUMEROENTERO.toString());
+                System.out.println(dato.charAt(i));
+                cadenaEnteros = cadenaEnteros + dato.charAt(i);
+                System.out.println(cadenaEnteros);
+
+            }*/
+            if (caracter.equals("1") || caracter.equals("2") || caracter.equals("3") || caracter.equals("4") || caracter.equals("5") || caracter.equals("6") || caracter.equals("7") || caracter.equals("8")
+                    || caracter.equals("9") || caracter.equals("0")) {
+
+                System.out.println(TipoToken.NUMEROENTERO.toString());
+                System.out.println(dato.charAt(i));
+                cadenaEnteros = cadenaEnteros + dato.charAt(i);
+                cadenaFEnteros = cadenaFEnteros + cadenaEnteros;
+                System.out.println(cadenaEnteros);
 
             }
+
         }
 
-        Interfaz.salida.setText(TipoToken.CARACTER.getTipo() + " " + cadenaCaracteres + "\n" + TipoToken.SIMBOLO.getTipo() + " " + cadenaSimbolos + "\n" + TipoToken.NUMEROENTERO.getTipo() + cadenaEnteros + "\n" + TipoToken.ID.toString() + cadenaId);
+        Interfaz.salida.setText(TipoToken.CARACTER.getTipo() + " " + cadenaFCaracteres + "\n" + TipoToken.SIMBOLO.getTipo() + " " + cadenaFSimbolos + "\n" + TipoToken.NUMEROENTERO.getTipo() + cadenaFEnteros + "\n" + TipoToken.ID.toString() + cadenaFId);
 
+    }
+
+    public static boolean validarNumero(String dato) {
+
+        if (dato == null || dato.length() == 0) {
+            return false;
+        }
+        try {
+            Double.parseDouble(dato);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
     }
 
 }
